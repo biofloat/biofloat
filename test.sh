4a03bb09e84acb1647bea406f2a14df418283d2b
Mike McCann <MBARIMike@gmail.com>
2015-11-11 16:35:57 -0800

    Use renamed load_biofloat_cache.py script, test after .dropna()

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-coverage run --source=biofloat scripts/load_cache.py --age 3000 --profiles 1
+coverage run --source=biofloat scripts/load_biofloat_cache.py --age 3000 --profiles 1
 coverage run -a --source=biofloat tests/unit_tests.py
 coverage report -m
 
