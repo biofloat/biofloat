a2db35e5c67a271482f2b166a84142be94a16194
Mike McCann <MBARIMike@gmail.com>
2015-11-16 15:31:33 -0800

    Bump to version 0.2.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "biofloat",
-    version = "0.1.19",
+    version = "0.2.0",
     packages = find_packages(),
     requires = ['Python (>=2.7)'],
     install_requires = [
