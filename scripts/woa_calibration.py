d98316bb0b3843d321dd9fa47a9ad6a9cd31980c
Mike McCann <MBARIMike@gmail.com>
2015-11-23 13:57:07 -0800

    Implemented --results_file saving, added --print_woa_lookups option, factored out woa_lookup() method

--- scripts/woa_calibration.py
+++ scripts/woa_calibration.py
@@ -7,6 +7,7 @@ sys.path.insert(0, parent_dir)
 
 import logging
 import matplotlib as plt
+import pandas as pd
 import xray
 
 from biofloat import ArgoData
@@ -34,6 +35,24 @@ class WOA_Calibrator(object):
         gdf[['gain']].unstack(level=0).plot()
 
 
+    def woa_lookup(self, df):
+        '''Given a DataFrame of profile data for an Argo float as read from
+        a biofloat cache file, average the data to the spatial temporal
+        grid of the World Ocean Atlas and return a DataFrame with float
+        and WOA O2 saturation columns added.  The WOA lookup goes across
+        the Internet so can take a minute or so to lookup all the values.
+        '''
+        sdf = surface_mean(df)
+        sdf = add_columns_for_groupby(sdf)
+        msdf = monthly_mean(sdf)
+        msdf = add_columns_for_woa_lookup(msdf)
+        self.logger.info('Doing WOA lookup for %s points', len(msdf))
+        woadf = add_column_from_woa(msdf, verbose=(self.args.print_woa_lookups 
+                                                   and self.args.verbose))
+
+        gdf = calculate_gain(woadf)
+
+        return gdf
 
     def process(self):
         self.logger.setLevel(self._log_levels[self.args.verbose])
@@ -45,22 +64,26 @@ class WOA_Calibrator(object):
         else:
             wmo_list = ad.get_cache_file_oxy_count_df()['wmo'].tolist()
 
-        for wmo in wmo_list:
-            self.logger.info('Reading float %s from %s', wmo, self.args.cache_file)
-            df = ad.get_float_dataframe([wmo],
-                                        max_profiles=self.args.profiles, 
-                                        max_pressure=self.args.pressure)
+        self.logger.info('Reading float profile data from %s', self.args.cache_file)
+        for i, wmo in enumerate(wmo_list):
+            self.logger.info('WMO_%s: Float %s of %s', wmo, i+1, len(wmo_list))
+            try:
+                with pd.HDFStore(self.args.results_file) as s:
+                    self.logger.debug('Reading %s from %s', ('/WOA_WMO_{}').format(wmo), self.args.cache_file)
+                    wmo_gdf = s.get(('/WOA_WMO_{}').format(wmo))
+                    self.logger.debug('Done.')
+            except KeyError:
+                df = ad.get_float_dataframe([wmo],
+                                            max_profiles=self.args.profiles, 
+                                            max_pressure=self.args.pressure)
+                wmo_gdf = self.woa_lookup(df)
 
-            sdf = surface_mean(df)
-            sdf = add_columns_for_groupby(sdf)
-            msdf = monthly_mean(sdf)
-            msdf = add_columns_for_woa_lookup(msdf)
-            self.logger.info('Doing WOA lookup for %s points', len(msdf))
-            woadf = add_column_from_woa(msdf, verbose=self.args.verbose)
-            gdf = calculate_gain(woadf)
+                # Save intermediate results to HDF file so that the script can
+                # pick up where it left off following network or other problems
+                with pd.HDFStore(self.args.results_file) as s:
+                    s.put(('/WOA_WMO_{}').format(wmo), wmo_gdf)
 
-            self.logger.info('Gain for %s = %s', wmo, gdf.groupby('wmo').gain.mean().values[0])
-            # TODO: Save to HDf file
+            self.logger.info('Gain for %s = %s', wmo, wmo_gdf.groupby('wmo').gain.mean().values[0])
 
     def process_command_line(self):
         import argparse
@@ -86,8 +109,10 @@ class WOA_Calibrator(object):
                                      help='Maximum number of profiles to read in')
         parser.add_argument('--pressure', action='store', 
                                      help='Maximum pressure to read in')
-        parser.add_argument('--results_file', action='store', 
-                                     help='File name to save df of the results')
+        parser.add_argument('--print_woa_lookups', action='store_true', 
+                                     help='In conjunction with -v print WOA lookups')
+        parser.add_argument('--results_file', action='store', required=True,
+                             help='File name for float and woa surface saturation values')
         parser.add_argument('-v', '--verbose', nargs='?', choices=[0,1,2,3], type=int,
                             help='0: ERROR, 1: WARN, 2: INFO, 3:DEBUG', default=0, const=2)
 
