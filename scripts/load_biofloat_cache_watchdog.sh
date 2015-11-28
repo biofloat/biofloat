9f681bb523bf37b51f5667b837f8e65b444148c0
Mike McCann <MBARIMike@gmail.com>
2015-11-28 09:49:14 -0800

    Include  in email message, send email on Finished

--- scripts/load_biofloat_cache_watchdog.sh
+++ scripts/load_biofloat_cache_watchdog.sh
@@ -5,10 +5,12 @@
 # are: <log_file_name> <load_biofloat_cache.py command with all arguments>.
 #
 # Execute from cron like:
-# * */10 * * *  load_biofloat_cache_watchdog.sh /u/mccann/dev/biofloatgit/cache_age364_MR_dropna.out /u/mccann/dev/biofloatgit/scripts/load_biofloat_cache.py --age 364 --cache_dir /data/biofloat -v
+# * */10 * * *  load_biofloat_cache_watchdog.sh /u/mccann/dev/biofloatgit/cache_age364_MR_dropna.out /u/mccann/dev/biofloatgit/scripts/load_biofloat_cache.py --age 364 --cache_dir /data/biofloat -v > /dev/null 2>&1
 
+######## Change these for your installation
 email_address=mccann
 venv_dir=/u/mccann/dev/biofloatgit/venv-biofloat
+########
 
 log_file=$(echo $@ | cut -d ' ' -f 1)
 shift
@@ -24,7 +26,8 @@ log_file_age=$((ct - ft))
 if [ $log_file_age -gt 600 ]
 then
     echo Paused
-    echo "$lbc_cmd  has paused.  Attempting to kill and restart." | mail -s $log_file $email_address
+    last_line=$(tail -1 $log_file)
+    echo "$lbc_cmd has paused.  Attempting to kill and restart.\n$last_line" | mail -s $log_file $email_address
     # Can't seem to grep for $lbc_args, grep for load_biofloat_cache.py instead
     ##pid=$(ps -ef | grep "\"$lbc_args\"" | awk -F' ' '{print $2}')
     pid=$(ps -ef | grep load_biofloat_cache.py | grep -v bash | grep -v grep | awk -F' ' '{print $2}')
@@ -33,12 +36,14 @@ then
         echo Killing $pid
         kill $pid
     fi
-    last_line=$(tail -1 $log_file | cut -d ' ' -f 1)
-    if [ ! "$last_line" == "Finished" ]
+    first_word=$(echo $last_line | cut -d ' ' -f 1)
+    if [ ! "$first_word" == "Finished" ]
     then
         echo Restarting python $lbc_cmd 
         source $venv_dir/bin/activate
         python $lbc_cmd >> $log_file 2>&1 &
+    else
+        echo "$lbc_cmd appears to have finished. You can cancel this cron job." | mail -s $log_file $email_address
     fi
 fi
 
