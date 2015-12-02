100de2d547741b20a291d85f2d14157ab3f802ad
Mike McCann <MBARIMike@gmail.com>
2015-12-01 16:20:30 -0800

    With variables added to cache file name bump to version 0.4.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "biofloat",
-    version = "0.3.2",
+    version = "0.4.0",
     packages = find_packages(),
     requires = ['Python (>=2.7)'],
     install_requires = [
